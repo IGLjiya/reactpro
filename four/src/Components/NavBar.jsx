32ebe3d30162bb348b5dbca738eec872b5bd2fba
IGLjiya <althfrhmn@gmail.com>
2024-09-26 12:57:01 +0530

    12

--- four/src/Components/NavBar.jsx
+++ four/src/Components/NavBar.jsx
@@ -1,42 +1,117 @@
 import React from 'react';
-import { Button, Col, Container, Form, Nav, Navbar, Row } from 'react-bootstrap';
-import { BsCart3 } from "react-icons/bs";
-import { CiUser } from "react-icons/ci";
-
+import { Button, Col, Container, Dropdown, Form, Nav, Navbar, NavItem, NavLink, Row } from 'react-bootstrap';
+import { GoSearch } from "react-icons/go";
+import { FaOpencart, FaRegUser, FaUserCircle } from "react-icons/fa";
+import { MdFavoriteBorder, MdGetApp } from "react-icons/md";
+import './Navbar.css'
+import { CiUser } from 'react-icons/ci';
 
 const NavBar = () => {
     return (
         <div>
-            <Navbar expand="lg" className="bg-danger sticky-top">
+            <div style={{ backgroundColor: 'rgb(154, 15, 45)', color: 'white', height: '30px' }}>
+                <div className='container d-flex justify-content-between'>
+                    <div><p style={{ fontSize: '13px', marginTop: '5px' }}>Brand Waali Quality, Bazaar Waali Deal!</p></div>
+                    <div>
+                        <a href="" className='mx-2 text-decoration-none text-light'>Impact@Snapdeal</a>
+                        <a href="" className='mx-2 text-decoration-none text-light'>Help Center</a>
+                        <a href="" className='mx-2 text-decoration-none text-light'>Sell On Snapdeal</a>
+                        <a href="" className='mx-2 text-decoration-none text-light'><MdGetApp className='fs-5' />Download App</a>
+                    </div>
+                </div>
+            </div>
+            <Navbar sticky='top' className="bg-danger" >
                 <Container fluid>
                     <Navbar.Brand href="#" className='ms-5 ps-5'><img src='https://i3.sdlcdn.com/img/snapdeal/darwin/logo/sdLatestLogo.svg'></img></Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
-                    <Form className="d-flex ms-5" style={{width:"50%"}}>
+                        <Form className="d-flex ms-5 me-5" style={{ width: "50%" }}>
                             <Form.Control
                                 type="search"
                                 placeholder="Search"
                                 className="me-2"
                                 aria-label="Search"
-                                style={{borderRadius:"0px"}}
+                                style={{ borderRadius: "0px" }}
                             />
-                            <Button variant="dark" style={{marginLeft:-10, borderRadius:"0px", width:"25%"}}>Search</Button>
+                            <Button variant="dark" style={{ marginLeft: -10, borderRadius: "0px", width: "25%" }}>Search</Button>
                         </Form>
-                        <Nav
+                        {/* <Nav
                             className="me-auto m-5 p-3 my-2 my-lg-0"
                             style={{ maxHeight: '100px',position:'absolute',right:'3%',top:'',}}
                             navbarScroll>
                                 <Row className='justify-content-end text-end text-light'>
                                     <Col className='text-center text-light gap-2 d-flex' style={
                                         {lineHeight:'15px '}
-                                    }>Cart<BsCart3/></Col>
+                                    }>Cart</Col>
                                     <Col className='justify-content-center'><CiUser size={23} /></Col>
                                 </Row>
-                        </Nav>
+                        </Nav> */}
                        
+                       <Navbar.Text className='col-1 px-3 py-3 ms-5 ' style={{ borderLeft: '1px solid grey', borderRight: '1px solid grey', }}>
+                            <a href='#' className='text-white d-flex'><h6>Cart</h6><FaOpencart className='fs-3 mx-1 w-50' /></a>
+                        </Navbar.Text>
+                        <Dropdown as={NavItem} className='col-1 py-3'>
+                            <Dropdown.Toggle as={NavLink} className='text-white'>Sign In <FaUserCircle className='fs-3 ms-3' /></Dropdown.Toggle>
+                            <Dropdown.Menu style={{ backgroundColor: 'rgb(47, 46, 46)', paddingLeft: '10px', paddingRight: '10px' }}>
+                                <Dropdown.Item style={{ color: 'white' }}><FaRegUser />Your Account</Dropdown.Item>
+                                <Dropdown.Item style={{ color: 'white' }}><MdFavoriteBorder /> Your Orders</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.ItemText style={{ color: 'white', textAlign: 'center' }}>
+                                    <span style={{ fontSize: '12px', color: 'grey' }}>If you are new user</span><br />Register
+                                    <Button type="submit" className=' border-0 px-5' style={{ borderRadius: '0%', backgroundColor: 'rgb(217, 21, 64)' }}>LOGIN</Button>
+                                </Dropdown.ItemText>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                       
+
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            {/* <div style={{backgroundColor:'rgb(154, 15, 45)',color:'white',height:'30px'}}>
+                <div className='container d-flex justify-content-between'>
+                    <div><p style={{fontSize:'13px',marginTop:'5px'}}>Brand Waali Quality, Bazaar Waali Deal!</p></div>
+                    <div>
+                        <a href="" className='mx-2'>Impact@Snapdeal</a>
+                        <a href="" className='mx-2'>Help Center</a>
+                        <a href="" className='mx-2'>Sell On Snapdeal</a>
+                        <a href="" className='mx-2'><MdGetApp className='fs-5'/>Download App</a>
+                    </div>
+                </div>
+            </div>
+            <Navbar sticky="top" style={{height:'80px',backgroundColor:'rgb(217, 21, 64)'}} >
+                <Container>
+                    <Navbar.Brand className='col-2'><img src='https://i3.sdlcdn.com/img/snapdeal/darwin/logo/sdLatestLogo.svg' width={'100%'} height={40}/></Navbar.Brand>
+                    <Form className='col-7 ms-5 d-flex' >
+                            <Col xs="7">
+                                <Form.Control
+                                type="text"
+                                placeholder="Search products&brands"
+                                style={{borderRadius:'0%',height:'40px',paddingLeft:'10%'}}
+                                />
+                            </Col>
+                            <Col xs="3">
+                                <Button type="submit" className=' border-0 py-2 px-4 d-flex' style={{borderRadius:'0%',background:'rgb(47, 46, 46)',height:'40px'}}>
+                                    <GoSearch className='m-1'/> <h6>Search</h6>
+                                    </Button>
+                            </Col>
+                    </Form>
+                    <Navbar.Text className='col-1 px-3 py-3 ' style={{borderLeft:'1px solid grey',borderRight:'1px solid grey'}}>
+                        <a href='#' className='text-white d-flex'><h6>Cart</h6><FaOpencart className='fs-3 mx-1 w-50' /></a>
+                    </Navbar.Text>
+                    <Dropdown as={NavItem} className='col-1 py-3'>
+                        <Dropdown.Toggle as={NavLink} className='text-white'>Sign In <FaUserCircle className='fs-3 ms-3'/></Dropdown.Toggle>
+                        <Dropdown.Menu style={{backgroundColor:'rgb(47, 46, 46)',paddingLeft:'10px',paddingRight:'10px'}}>
+                            <Dropdown.Item style={{color:'white'}}><FaRegUser />Your Account</Dropdown.Item>
+                            <Dropdown.Item style={{color:'white'}}><MdFavoriteBorder /> Your Orders</Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.ItemText style={{color:'white',textAlign:'center'}}>
+                                <span style={{fontSize:'12px',color:'grey'}}>If you are new user</span><br />Register
+                                <Button type="submit" className=' border-0 px-5' style={{borderRadius:'0%',backgroundColor:'rgb(217, 21, 64)'}}>LOGIN</Button>
+                            </Dropdown.ItemText>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Container>
+            </Navbar> */}
         </div>
     );
 }
--- four/src/Components/SecondPage.jsx
+++ four/src/Components/SecondPage.jsx
@@ -3,6 +3,7 @@ import { Button, Card, Carousel, Col, Container, Form, Row } from 'react-bootstr
 import myImgPincode from './img/pincode.png'
 import './SecondPage.css'
 import { FaApple, FaGooglePlay } from "react-icons/fa6";
+import { CiSearch } from "react-icons/ci";
 
 
 
@@ -22,11 +23,38 @@ const SecondPage = () => {
                 </div>
                 <div style={{ position: '' }}></div> */}
 
-                <div class="container2">
-                    <div class="item item1">
-                        <div className='bg-danger' style={{ width: '150px', height: '800px' }}>side</div>
+                <div className="container2">
+                    <div className="item item1 ">
+                        {/* <div className='bg-danger' style={{ width: '150px', height: '800px' }}></div>   style={{backgroundImage:"url('https://n1.sdlcdn.com/imgs/k/v/x/Men_sitenavigation-b972a.jpg') */}
+                        <div><h6>TOP CATEGORIES</h6></div>
+                        <div className='d-flex flex-column flex-wrap pe-2  text-truncate ' style={{width:"150px"}}>
+                        <li className='list-unstyled py-2'><a className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate'> <img src="https://n1.sdlcdn.com/imgs/k/v/x/Men_sitenavigation-b972a.jpg" width={28} height={28} alt="" /> Mens's Fashion</div></a></li>
+                        <li className='list-unstyled py-2'><a className='text-decoration-none text-dark text-truncate' href=""><div><img src="https://n1.sdlcdn.com/imgs/k/v/x/WoMen_sitenav-5a8ca.jpg" width={28} height={28} alt="" />Women's Fashion</div></a></li>
+                        <li className='list-unstyled py-2'><a className='text-decoration-none text-dark text-truncate' href=""><div><img src="https://n1.sdlcdn.com/imgs/k/v/x/HOme_sitenavigation-d7a00.jpg" width={28} height={28} alt="" />Home & Kitchen</div></a></li>
+                        <li className='list-unstyled py-2'><a className='text-decoration-none text-dark text-truncate' href=""><div><img src="https://n4.sdlcdn.com/imgs/k/v/x/Toys_Sitenavigation-ef666.jpg" width={28} height={28} alt="" />Toys, Kids' Fashion & more</div></a></li>
+                        <li className='list-unstyled py-2'><a className='text-decoration-none text-dark text-truncate' href=""><div><img src="https://n2.sdlcdn.com/imgs/k/v/x/Beauty_Site_navigation-5f3be.jpg" width={28} height={28} alt="" />Beauty, Health & Daily Needs</div></a></li>
+                        </div>
+                        <div><h6>MORE CATEGORIES</h6></div>
+                        <div className='d-flex flex-column flex-wrap text-truncate '>
+                        <li className='list-unstyled py-2'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate'>Automotive</div></a></li>
+                        <li className='list-unstyled py-2'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate'>Electronic</div></a></li>
+                        <li className='list-unstyled py-2'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate'>Mobile & Accessories</div></a></li>
+                        <li className='list-unstyled py-2'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate'>Sport, Fitness & outdoor</div></a></li>
+                        <li className='list-unstyled py-2'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate'>Computer & Gamming</div></a></li>
+                        <li className='list-unstyled py-2'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate'>Book, Media & Music</div></a></li>
+                        <li className='list-unstyled py-2'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate'>Hobbies</div></a></li>
+                        </div>
+                        <div><h6>TRENDING SEARCHES</h6></div>
+                        <div className='d-flex flex-column flex-wrap text-truncate '>
+                        <li className='list-unstyled'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate d-flex align-content-center'><CiSearch className='py-1' size={25}/> <p>Kitchen Product</p></div></a></li>
+                        <li className='list-unstyled'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate d-flex align-content-center'><CiSearch className='py-1' size={25}/> <p>Shoes For Men</p></div></a></li>
+                        <li className='list-unstyled'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate d-flex align-content-center'><CiSearch className='py-1' size={25}/> <p> Kurti Set</p></div></a></li>
+                        <li className='list-unstyled'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate d-flex align-content-center'><CiSearch className='py-1' size={25}/> <p>Sandal Men</p></div></a></li>
+                        <li className='list-unstyled'><a style={{fontSize:"15px"}} className='text-decoration-none text-dark text-truncate' href=""><div className='text-truncate d-flex align-content-center'><CiSearch className='py-1' size={25}/> <p>Sport Shoes Men</p></div></a></li>
+
+                        </div>
                     </div>
-                    <div class="item item2">
+                    <div className="item item2">
                         <Carousel style={{ position: 'relative' }}>
                             <Carousel.Item interval={2000}>
                                 <img style={{ width: '100%', height: '450px' }} src='https://n1.sdlcdn.com/imgs/a/b/c/feedConfig/landing_banner.jpg'></img>
@@ -42,7 +70,7 @@ const SecondPage = () => {
                             </Carousel.Item>
                         </Carousel>
                     </div>
-                    <div class="item item3">
+                    <div className="item item3">
                         <Card style={{ position: 'relative', top: '10%', }} >
                             <Card.Img variant="top" src={myImgPincode} width={20} height={200} />
                             <Card.Body>
@@ -57,146 +85,146 @@ const SecondPage = () => {
 
                         </Card>
                     </div>
-                    <div class="item item4">
+                    <div className="item item4">
                         <h4>RECENTLY VIWED PRODUCTS</h4>
-                        <div className="d-flex gap-4" >
+                        <div className="d-flex flex-shrink-0 gap-4" >
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://n2.sdlcdn.com/imgs/b/e/g/large/Tanishka-Brown-Door-Curtain-Set-SDL880259736-1-1a477.jpg" />
                                 <Card.Body>
                                     <Card.Text>
-                                        <h6>Tanishka Fabs Solid Semi-Transparent Eyelet Curtain</h6> <br/>
+                                        <h6>Tanishka Fabs Solid Semi-Transparent Eyelet Curtain</h6> <br />
                                         <del>Rs 999</del> Rs 520
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://n1.sdlcdn.com/imgs/k/o/m/large/Honey-Bunny-L-Diaper-Pants-SDL500431515-1-cfd6d.jpg" />
                                 <Card.Body>
                                     <Card.Text>
-                                        <h6>Honey Bunny Pants Diapers l-64pcs</h6> <br/>
+                                        <h6>Honey Bunny Pants Diapers l-64pcs</h6> <br />
                                         <del>Rs 999</del> Rs 581
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://n4.sdlcdn.com/imgs/k/p/m/large/Honey-Bunny-M-Diaper-Pants-SDL307366701-1-73ca3.jpg" />
                                 <Card.Body>
                                     <Card.Text>
-                                         <h6>Honey Bunny -M Dipers Pants</h6><br/>
+                                        <h6>Honey Bunny -M Dipers Pants</h6><br />
                                         <del>Rs 1099</del> Rs 554
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://n2.sdlcdn.com/imgs/k/y/z/large/GKBOSS-Indoor-Three-Layer-Lucky-SDL405421240-1-ad2b9.jpg" />
                                 <Card.Body>
                                     <Card.Text>
-                                        <h6>GKBOSS Indoor Three Layer Lucky Bamboo Palnt Plant</h6> <br/>
+                                        <h6>GKBOSS Indoor Three Layer Lucky Bamboo Palnt Plant</h6> <br />
                                         <del>Rs 699</del> Rs 199
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://n2.sdlcdn.com/imgs/k/o/4/large/Carnival-Banana-full-plate-6-SDL942540133-1-3ce50.PNG" />
                                 <Card.Body>
                                     <Card.Text>
-                                        <h6>Carnival - Banana full palte 6pcs Green Melamine Dinner Set</h6> <br/>
+                                        <h6>Carnival - Banana full palte 6pcs Green Melamine Dinner Set</h6> <br />
                                         <del>Rs 799</del> Rs 548
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
 
                         </div>
                     </div>
-                    <div class="item item5">
-                       <h4>TRENDING PRODUCTS</h4>
-                       <div className="d-flex gap-3">
-                       <Card style={{ width: '18rem' }}>
+                    <div className="item item5">
+                        <h4>TRENDING PRODUCTS</h4>
+                        <div className="d-flex gap-3">
+                            <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://t1.sdlcdn.com/imgs/i/n/r/large/bhawna-collection-Loard-Shiv-Trishul-SDL890408077-1-86933.jpeg" />
                                 <Card.Body>
                                     <Card.Text>
-                                        <h6>Shiv Trishul Damru Gol-plted Lochet with Rudraksha Mala</h6> <br/>
+                                        <h6>Shiv Trishul Damru Gol-plted Lochet with Rudraksha Mala</h6> <br />
                                         <del>Rs 599</del> Rs 105
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://t3.sdlcdn.com/imgs/i/u/v/large/HeelSocks1-aeb5a.jpg" />
                                 <Card.Body>
                                     <Card.Text>
-                                        <h6>Stay Healthy -Foot Protector</h6> <br/>
+                                        <h6>Stay Healthy -Foot Protector</h6> <br />
                                         <del>Rs 899</del> Rs 96
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://t3.sdlcdn.com/imgs/i/z/j/large/bc12-606ed.jpg" />
                                 <Card.Body>
                                     <Card.Text>
-                                        <h6>Maxbell USB Rechargeable Electronic Flameless Lighter</h6> <br/>
+                                        <h6>Maxbell USB Rechargeable Electronic Flameless Lighter</h6> <br />
                                         <del>Rs 899</del> Rs 172
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://t1.sdlcdn.com/imgs/j/d/1/large/Aadi-Black-Casual-Shoes-SDL461191496-3-fa710.jpeg" />
                                 <Card.Body>
                                     <Card.Text>
-                                        <h6>Aadi BLack Casual Shoes </h6> <br/>
+                                        <h6>Aadi BLack Casual Shoes </h6> <br />
                                         <del>Rs 999</del> Rs 331
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://t4.sdlcdn.com/imgs/k/w/k/large/Bentag-kitchenware-Vegetable-Fruit-Clever-SDL841893040-1-1e2e8.jpg" />
                                 <Card.Body>
                                     <Card.Text>
-                                        <h6>Bentag kitchenware Vegetable $ fruit Clever Cutter Stainless</h6> <br/>
+                                        <h6>Bentag kitchenware Vegetable $ fruit Clever Cutter Stainless</h6> <br />
                                         <del>Rs 499</del> Rs 139
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://t1.sdlcdn.com/imgs/k/e/u/large/Veirdo-100-Cotton-Regular-Fit-SDL051246309-1-51a91.jpg" />
                                 <Card.Body>
                                     <Card.Text>
-                                        <h6>Veirdo- Multicolor Cotton Regula Fit Man's T-Shirt </h6> <br/>
+                                        <h6>Veirdo- Multicolor Cotton Regula Fit Man's T-Shirt </h6> <br />
                                         <del>Rs 1199</del> Rs 291
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src="https://t3.sdlcdn.com/imgs/h/r/s/large/Shiv-Shakti-Kavach-5-Mukhi-SDL772359305-1-3b306.jpeg" />
                                 <Card.Body>
                                     <Card.Text>
-                                        <h6>Shiv Shakti Kavach 5 Mukhi Rudraksha Mala with Shiv</h6> <br/>
+                                        <h6>Shiv Shakti Kavach 5 Mukhi Rudraksha Mala with Shiv</h6> <br />
                                         <del>Rs 599</del> Rs 176
                                     </Card.Text>
-                                    
+
                                 </Card.Body>
                             </Card>
-                       </div>
+                        </div>
                     </div>
-                    <div class="item item6 d-flex">
+                    <div className="item item6 d-flex">
                         <div className="left">
                             <img src="https://i1.sdlcdn.com/img/appScreenshot@1x.png" width={430} height={462} alt="" />
                         </div>
                         <div className="right">
                             <h1>Download Snapdeal App Now</h1>
                             <p>Fast, Simple & Delightful. <br /> All it takes is 30 seconds to Download.</p>
-                            <button className='btn1 '><FaApple size={35}/> Download on the App Store</button>
-                            <button className='btn2'><FaGooglePlay size={35}/>Google Play</button>
+                            <button className='btn1 '><FaApple size={35} /> Download on the App Store</button>
+                            <button className='btn2'><FaGooglePlay size={35} />Google Play</button>
                         </div>
 
                     </div>
